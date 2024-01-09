ee71d47273c2afb4a7c54eb7e06b3013b6691ed5
Mari <charmee2928@gmail.com>
2024-01-09 03:10:02 +0000

    Delete Duplicates from Sorted List

---
+++ LinkedList/deleteDuplicates.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+
+        Input: head = [1,1,1,2,3]
+        Output: [2,3]
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                prev.next = head
+            else:
+                prev = prev.next
+                head = head.next
+        return dummy.next
