6138f1f60b4537db3cd445c7453e213ccc69dd5b
Mari <charmee2928@gmail.com>
2024-01-14 00:35:54 +0000

    Intersection of Two Linked Lists

---
+++ LinkedList/getIntersectionNode.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+
+        Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5]
+        Output: Intersected at '8'
+        """
+        seen = set()
+        while headA:
+            seen.add(headA)
+            headA = headA.next
+        while headB:
+            if headB in seen:
+                return headB
+            headB = headB.next
+        return None
