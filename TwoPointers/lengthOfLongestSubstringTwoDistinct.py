a31f35932ce677f6f47f2709487bfe26c1e6aa02
Mari <charmee2928@gmail.com>
2024-01-16 01:57:30 +0000

    Longest Substring With At Most Two Distinct Chars

--- TwoPointers/lengthOfLongestSubstringTwoDistinct.py
+++ TwoPointers/lengthOfLongestSubstringTwoDistinct.py
@@ -8,13 +8,13 @@ class Solution:
         start, res = 0, 0
         seen = collections.Counter()
 
-        for end in range(len(s)):
-            seen[s[end]] += 1
+        for end in range(len(s)): # 0, 1, 2, 3, 4
+            seen[s[end]] += 1       # [b: 1, a: 1]
             while len(seen) > 2:
-                seen[s[start]] -= 1
-                if seen[s[start]] == 0:
+                seen[s[start]] -= 1    
+                if seen[s[start]] == 0: # s[2] == e
                     del seen[s[start]]
-                start += 1
-            res = max(res, end - start + 1)
+                start += 1             # 1 -> 2 -> 3
+            res = max(res, end - start + 1) # 1 -> 2 -> 3 
         
         return res
